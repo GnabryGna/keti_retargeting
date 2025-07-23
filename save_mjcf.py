325cb1bf7bb3e6c409d95a72cdf94ba4b990953a
yoonki-hong <hyg516@gmail.com>
2025-07-23 09:50:55 +0900

    Update mesh inertia computation to 'exact'

--- save_mjcf.py
+++ save_mjcf.py
@@ -62,7 +62,6 @@ if __name__ == '__main__':
                              pos=[0, 0, -0.005])
     left_robot_arm_mount.add('site',
                              name='mount_site')
-
     left_robot_hand_xml_path = os.path.join(os.getcwd(), 'wonik_allegro', 'left_hand.xml')
     left_robot_hand = mjcf.from_path(left_robot_hand_xml_path)
     left_robot_hand_attachment_site = left_robot_arm_mount.find('site', 'mount_site')
@@ -83,7 +82,6 @@ if __name__ == '__main__':
                               pos=[0, 0, -0.005])
     right_robot_arm_mount.add('site',
                               name='mount_site')
-
     right_robot_hand_xml_path = os.path.join(os.getcwd(), 'wonik_allegro', 'right_hand.xml')
     right_robot_hand = mjcf.from_path(right_robot_hand_xml_path)
     right_robot_hand_attachment_site = right_robot_arm_mount.find('site', 'mount_site')
@@ -94,8 +92,9 @@ if __name__ == '__main__':
     # Barcode scanner
     barcode_scanner_xml_path = os.path.join(os.getcwd(), 'barcode_scanner', 'barcode_scanner.xml')
     barcode_scanner = mjcf.from_path(barcode_scanner_xml_path)
-    barcode_scanner.default.geom.priority = 1
+    barcode_scanner.default.mesh.inertia = 'exact'
     barcode_scanner.default.geom.condim = 6
+    barcode_scanner.default.geom.priority = 1
     barcode_scanner_attachment_frame = scene.attach(barcode_scanner)
     barcode_scanner_attachment_frame.add('freejoint')
     barcode_scanner_attachment_frame.pos = [0.45, 0.5, 0.2]
@@ -167,19 +166,29 @@ if __name__ == '__main__':
         [-0.00522, 0.005731, 0.047954, 0, 0, np.pi/2], # 010_potted_meat_can
         [0.031735, 0.01317, -0.073959, -np.pi/90, 0, 0] # 021_bleach_cleanser
     ]
-
+    # ycb_object_mass = [0.411, 0.514, 0.349, 0.603, 0.37, 1.131]
+    # ycb_object_inertia_matrix = [ # [xx, yy, zz, xy, xz, yz]
+    #     [5.8284476667e-03, 4.0928755029e-03, 2.4318893100e-03, -1.2156971874e-06, 3.7193151667e-05, 1.3268114999e-05],
+    #     [3.1530279732e-03, 2.6445788540e-03, 7.9379902551e-04, -1.8465066361e-05, 9.3739782990e-05, -3.1919166191e-05],
+    #     [1.0026548160e-03, 1.0105218561e-03, 5.4967554550e-04, -4.1885969743e-07,  -2.3207722080e-06, 1.0153299346e-06],
+    #     [2.4304633618e-03, 2.6748587639e-03, 7.0151123676e-04, 1.2578583989e-04,  -8.2488738081e-06, -1.1690060534e-05],
+    #     [7.0538916484e-04, 1.2091032282e-03, 9.1157704910e-04, -2.7839777344e-05, -1.5531186341e-06, 3.1372827657e-06],
+    #     [4.3546483321e-03, 4.6959742842e-03, 8.0486867445e-04, 3.5311987973e-06, -2.5203014495e-04, 2.1901421083e-06]
+    # ]
+    
     ycb_object_file_dir = os.path.join(os.getcwd(), 'ycb')
     ycb_object_name_list = os.listdir(ycb_object_file_dir)
-
     for id, ycb_object_name in enumerate(ycb_object_name_list):
         file_names = os.listdir(os.path.join(ycb_object_file_dir, ycb_object_name))
         xml_file_name = [file_name for file_name in file_names if file_name.lower().endswith('.xml')][0]
         ycb_object_xml_path = os.path.join(ycb_object_file_dir, ycb_object_name, xml_file_name)
         ycb_object = mjcf.from_path(ycb_object_xml_path)
-        ycb_object.default.geom.priority = 1
+        ycb_object.default.mesh.inertia = 'exact'
         ycb_object.default.geom.condim = 6
-        ycb_object.default.geom.solimp = [1, 1, 0.001, 0.5, 2]
-
+        ycb_object.default.geom.priority = 1
+        # TODO: need to adjust
+        ycb_object.default.geom.solimp = [1, 1, 0.001, 0.5, 2] # default: [0.9, 0.95, 0.001, 0.5, 2]
+        
         # Add barcode site
         ycb_object_body = ycb_object.find('body', f'{ycb_object_name}')
         ycb_object_body.add('site',
@@ -189,18 +198,24 @@ if __name__ == '__main__':
                             rgba=[1, 0, 0, 1],
                             pos=ycb_object_barcode_pose[id][:3],
                             euler=ycb_object_barcode_pose[id][3:])
+        
+        # Set mass and inertia
+        # ycb_object_body.add('inertial',
+        #                     pos=[0, 0, 0],
+        #                     mass=ycb_object_mass[id],
+        #                     fullinertia=ycb_object_inertia_matrix[id])
 
         ycb_object_attachment_frame = scene.attach(ycb_object)
         ycb_object_attachment_frame.add('freejoint')
         ycb_object_attachment_frame.pos = ycb_object_init_pose[id][:3]
         ycb_object_attachment_frame.euler = ycb_object_init_pose[id][3:]
-    
+
     # Option
     scene.option.integrator = 'implicitfast'
     scene.option.cone = 'elliptic'
-    # scene.option.impratio = 10 # allegro default setting
     scene.option.flag.multiccd = 'enable'
-
+    scene.compiler.inertiagrouprange = [0, 2]
+    
     # Initial state (allegro)
     initial_state = scene.keyframe.add('key')
     initial_state.name = 'initial_state'
@@ -220,6 +235,5 @@ if __name__ == '__main__':
     )
     initial_state.ctrl = initial_state.qpos[:46]
 
-    mjcf.export_with_assets(mjcf_model=scene,
-                            out_dir= os.path.join(os.getcwd(), 'output'))
+    mjcf.export_with_assets(mjcf_model=scene, out_dir= os.path.join(os.getcwd(), 'output'))
     print('Complete')
